5c439887f6ed3d998fdad3d5594eeb207a9122a9
MilesCranmerBot <miles.cranmer.bot@gmail.com>
2026-07-26 16:48:08 +0100

    perf: narrow default precompile workload
    Co-authored-by: Miles Cranmer <miles.cranmer@gmail.com>

--- src/precompile.jl
+++ src/precompile.jl
@@ -33,7 +33,7 @@ end
 """`mode=:precompile` will use `@precompile_*` directives; `mode=:compile` runs."""
 function do_precompilation(::Val{mode}) where {mode}
     @maybe_setup_workload mode begin
-        for T in [Float32, Float64], nout in 1:2
+        for T in (Float32,), nout in (1,)
             start = nout == 1
             N = 30
             X = randn(T, 3, N)
